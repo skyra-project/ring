1c1473b554f53b68e81186598c962bbce38ce155
Aura Román <kyradiscord@gmail.com>
2023-12-27 18:21:38 +0100

    refactor: use /shared `registerCommands`

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { setup } from '#lib/setup/all';
-import { registerCommands } from '#lib/utilities/register-commands';
 import { envParseInteger, envParseString } from '@skyra/env-utilities';
 import { Client, container } from '@skyra/http-framework';
 import { init, load } from '@skyra/http-framework-i18n';
-import '@skyra/shared-http-pieces/register';
+import { registerCommands } from '@skyra/shared-http-pieces';
 import { createBanner } from '@skyra/start-banner';
 import gradient from 'gradient-string';
 
